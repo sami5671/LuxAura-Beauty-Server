3d3affb2d3c5a22c94cd38378a7bf2d3dae83e4d
sami5671 <samialam5671@gmail.com>
2023-10-21 13:45:24 +0600

    update

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // =================================================================
-// username: cosmeticAndBeautyProduct
-// password: qWUtivsREY8fN258
+
 // ===========================mongoDB section======================================
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmvmv30.mongodb.net/?retryWrites=true&w=majority`;
